00f6327b04356dfed7f4b2b45c5a0d45d37a4134
Macbit <puzerjov@gmail.com>
2015-10-06 08:21:20 +0100

    toggle successfully fixed

--- public/javascripts/js/scripts.js
+++ public/javascripts/js/scripts.js
@@ -1,8 +1,8 @@
 // Empty JS for your own code to be here
 
 $(document).ready(function(){
-    $("div.toggle").hide();
-    $("button.btn-primary").click(function(){
+    $("div.toggle").toggle();
+    $(".btn-primary").click(function(){
         $("div.toggle").toggle(1000);
     });
 });
