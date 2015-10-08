5a6f2ca422407a634298f7d01f4030f03ecb6829
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-09 00:57:12 +0100

    Changes

--- public/javascripts/js/scripts.js
+++ public/javascripts/js/scripts.js
@@ -1,42 +1,38 @@
-// Empty JS for your own code to be here
+var ts;
+
 function isUrlValid(url) {
     return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
 }
 
 $(document).ready(function () {
 
-            $("div.toggle").toggle();
-            $(".btn-primary").click(function () {
+    $("div.toggle").toggle();
+    $(".btn-primary").click(function () {
 
-                    if (!isUrlValid($('input').val()) || $('input').val().indexOf("path") == -1) {
-                            $($('input')).parents('div').addClass('has-warning');
-                            alert("Your link is not correct, please try again.");
-                        } else {
-                            $("div.toggle").toggle(1000);
-                        }
-                    });
+        if (!isUrlValid($('input').val()) || $('input').val().indexOf("path") == -1) {
+            $($('input')).parents('div').addClass('has-warning');
+            alert("Your link is not correct, please try again.");
+        } else {
+            $("div.toggle").toggle(1000);
+        }
+    });
 
 
+    $("#google").click(function () {
+        var url = $("#url_input").val();
+        $.get("/download/csv", {url: url}, function (response) {
+            ts = response.ts;
+            window.location.href = '/timetable_' + ts + '.csv';
+        });
 
-                $("#google").click(function () {
-                    var url = $("#url_input").val();
-                    $.get("/download/csv", {
-                        url: url
-                    }, function (response) {
-                        console.log("from front", response);
-                        window.location.href = '/timetable.csv';
-                    });
+    });
 
-                });
+    $("#apple").click(function () {
+        var url = $("#url_input").val();
+        $.get("/download/ics", {url: url}, function (response) {
+            ts = response.ts;
+            window.location.href = '/timetable_' + ts + '.ics';
+        });
 
-                $("#apple").click(function () {
-                    var url = $("#url_input").val();
-                    $.get("/download/ics", {
-                        url: url
-                    }, function () {
-                        console.log("download performed");
-                        window.location.href = '/download/ics';
-                    });
-
-                });
-            });
+    });
+});
