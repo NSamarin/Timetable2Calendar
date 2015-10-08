8a77b7f6abeb2a7816df8eee07e915019a716f50
Macbit <puzerjov@gmail.com>
2015-10-09 00:41:00 +0100

    checking input url field for zero

--- public/javascripts/js/scripts.js
+++ public/javascripts/js/scripts.js
@@ -1,27 +1,38 @@
 // Empty JS for your own code to be here
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("div.toggle").toggle();
-    $(".btn-primary").click(function(){
-        $("div.toggle").toggle(1000);
+    $(".btn-primary").click(function () {
+
+        if ($('input').val().length === 0) {
+            $($('input')).parents('div').addClass('has-warning');
+            alert("Your link is not correct, please try again.");
+        } else {
+            $("div.toggle").toggle(1000);
+        }
     });
 
-    $("#google").click(function() {
+
+
+    $("#google").click(function () {
         var url = $("#url_input").val();
-        $.get( "/download/csv", {url: url}, function(response) {
+        $.get("/download/csv", {
+            url: url
+        }, function (response) {
             console.log("from front", response);
             window.location.href = '/timetable.csv';
         });
 
     });
 
-    $("#apple").click(function() {
+    $("#apple").click(function () {
         var url = $("#url_input").val();
-        $.get( "/download/ics", {url: url}, function() {
+        $.get("/download/ics", {
+            url: url
+        }, function () {
             console.log("download performed");
             window.location.href = '/download/ics';
         });
 
     });
 });
-
