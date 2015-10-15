a5408b49e9969b9f851f58d873818c668f921b7e
Macbit <puzerjov@gmail.com>
2015-10-15 10:54:36 +0100

    front end refined

--- public/javascripts/js/scripts.js
+++ public/javascripts/js/scripts.js
@@ -9,13 +9,20 @@ $(document).ready(function () {
         if ($('input').val().indexOf("browser") == -1) {
             $($('input')).parents('div').addClass('has-warning');
             alert("Your link is not correct, please try again.");
+            return false;
         } else {
-            $("div.toggle").toggle(1000);
+            $("div.toggle").toggle(700);
+            return false;
+
         }
+
     });
 
 
     $("#google").click(function () {
+        
+        $('<p class="text-success">Please wait, downloading will start in 5 sec...</p>').appendTo('#intro');
+        console.log('google pressed');
         var url = $("#url_input").val();
         $.get("/download/csv", {
             url: url
@@ -27,6 +34,7 @@ $(document).ready(function () {
     });
 
     $("#apple").click(function () {
+        $("#intro").text("Downloading will start in 5 sec...");
         var url = $("#url_input").val();
         $.get("/download/ics", {
             url: url
@@ -73,6 +81,8 @@ $("#apple").click(function () {
 });
 
 
+
+
 //google analytics
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
@@ -90,12 +100,42 @@ ga('create', 'UA-68681929-1', 'auto');
 ga('send', 'pageview');
 
 //facebook like/share button
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=1917246968500417";
-  fjs.parentNode.insertBefore(js, fjs);
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=1917246968500417";
+    fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+//<!-- Yandex.Metrika counter -->
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function () {
+        try {
+            w.yaCounter33045043 = new Ya.Metrika({
+                id: 33045043,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });
+        } catch (e) {}
+    });
 
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () {
+            n.parentNode.insertBefore(s, n);
+        };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else {
+        f();
+    }
+})(document, window, "yandex_metrika_callbacks");
+//<!-- /Yandex.Metrika counter -->
